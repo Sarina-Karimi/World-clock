1ba9defcfd9126f1e03eecb837e274591d2b2e05
Sarina-Karimi <sarinakariimi@gmail.com>
2026-04-05 21:13:15 +0330

    fixed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import moment from "moment";
-import moment from "moment-timezone";
+
+
 function updateTime() {
     let losAngelesElement = document.querySelector("#los-angeles");
-    let losAngelesDateElement = losAngelesElement.querySelector("#date");
-    let losAngelesTimeElement = losAngelesElement.querySelector("#time");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
     let losAngelesTime = moment().tz("America/Los-Angeles")
     losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
     losAngelesTimeElement.innerHTML = losAngelesTime.format("hh:mm:ss [<small>A</small>] ");
 
+    let parisElement = document.querySelector("#paris");
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris")
+    parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format("hh:mm:ss [<small>A</small>] ");
 }
 
 updateTime();
