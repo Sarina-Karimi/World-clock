cd7a80aacb522822775b8bb8534b7143127c2053
Sarina-Karimi <sarinakariimi@gmail.com>
2026-04-06 12:07:30 +0330

    added a link
    added a link at the bottom to get back to the first page

--- src/index.js
+++ src/index.js
@@ -34,7 +34,8 @@ function updateCity(event) {
             <div class="date">${cityTime.format("MMMM Do YYYY")} </div>
         </div>
             <div class="time">${cityTime.format("hh:mm:ss ")}<small>${cityTime.format("A")}</small></div>
-     </div>                 
+     </div> 
+           <a href="/">All cities</a>          
 `;
 }
 
